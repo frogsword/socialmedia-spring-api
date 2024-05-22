185f9426aa057c748d99afd888ee2adf02e7c6e5
frogsword <113356699+frogsword@users.noreply.github.com>
2024-05-22 16:07:37 -0500

    added endpoint to like tweet

--- src/main/java/com/app/socialmedia/controllers/TweetController.java
+++ src/main/java/com/app/socialmedia/controllers/TweetController.java
@@ -3,7 +3,9 @@ package com.app.socialmedia.controllers;
 import com.app.socialmedia.dtos.tweet.TweetDto;
 import com.app.socialmedia.models.Tweet;
 import com.app.socialmedia.models.User;
+import com.app.socialmedia.repositories.UserRepository;
 import com.app.socialmedia.services.TweetService;
+import com.app.socialmedia.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,9 +17,11 @@ import java.util.List;
 @RequestMapping("api/tweets")
 public class TweetController {
     private final TweetService tweetService;
+    private final UserService userService;
 
-    public TweetController(final TweetService tweetService) {
+    public TweetController(final TweetService tweetService, UserService userService) {
         this.tweetService = tweetService;
+        this.userService = userService;
     }
 
     @PostMapping("/create")
@@ -30,7 +34,52 @@ public class TweetController {
         return ResponseEntity.ok(createdTweet);
     }
 
-    @GetMapping("/user/{userId}/all")
+    @PutMapping("/{tweetId}/like")
+    public ResponseEntity<String> likeTweet(@PathVariable String tweetId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = (User) authentication.getPrincipal();
+
+        List<String> likedTweets = currentUser.getLikedTweets();
+        boolean successful;
+
+        //unlike tweet
+        if (likedTweets.contains(tweetId)) {
+            //successful = userService.unlikeTweet(tweetId, currentUser.getId());
+            successful = userService.updateLikedTweets(tweetId, currentUser.getId(), true);
+            if (successful) {
+                boolean successfulLike = tweetService.updateLikeCount(tweetId, -1);
+                if (successfulLike) {
+                    return ResponseEntity.ok(tweetId);
+                }
+                else {
+                    return ResponseEntity.notFound().build();
+                }
+            }
+            else {
+                return ResponseEntity.notFound().build();
+            }
+        }
+
+        //like tweet
+        else {
+            //successful = userService.likeTweet(tweetId, currentUser.getId());
+            successful = userService.updateLikedTweets(tweetId, currentUser.getId(), false);
+            if (successful) {
+                boolean successfulLike = tweetService.updateLikeCount(tweetId, 1);
+                if (successfulLike) {
+                    return ResponseEntity.ok(tweetId);
+                }
+                else {
+                    return ResponseEntity.notFound().build();
+                }
+            }
+            else {
+                return ResponseEntity.notFound().build();
+            }
+        }
+    }
+
+    @GetMapping("/user/{userId}")
     public ResponseEntity<List<Tweet>> getAllTweets(@PathVariable String userId) {
         List<Tweet> tweets = tweetService.getUserTweets(userId);
         return ResponseEntity.ok(tweets);
