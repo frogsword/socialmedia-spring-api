235270273f32a8b125fa702ad207a99eb8e01f4b
frogsword <113356699+frogsword@users.noreply.github.com>
2024-05-22 16:08:26 -0500

    added username update function

--- src/main/java/com/app/socialmedia/services/UserService.java
+++ src/main/java/com/app/socialmedia/services/UserService.java
@@ -1,10 +1,12 @@
 package com.app.socialmedia.services;
 
+import com.app.socialmedia.models.Tweet;
 import com.app.socialmedia.models.User;
 import com.app.socialmedia.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,5 +18,62 @@ public class UserService {
 
     public List<User> allUsers() {
         return new ArrayList<>(userRepository.findAll());
+    }
+
+    public boolean likeTweet(String tweetId, String userId) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            List<String> likedTweets = user.getLikedTweets();
+            likedTweets.add(tweetId);
+            user.setLikedTweets(likedTweets);
+            userRepository.save(user);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean unlikeTweet(String tweetId, String userId) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            List<String> likedTweets = user.getLikedTweets();
+            likedTweets.remove(tweetId);
+            user.setLikedTweets(likedTweets);
+            userRepository.save(user);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean updateLikedTweets(String tweetId, String userId, boolean toRemove) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            List<String> likedTweets = user.getLikedTweets();
+
+            if (toRemove) {
+                likedTweets.remove(tweetId);
+            }
+            else {
+                likedTweets.add(tweetId);
+            }
+
+            user.setLikedTweets(likedTweets);
+            userRepository.save(user);
+            return true;
+        }
+        return false;
+    }
+
+    public User updateUsername(String userId, String newUsername) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            user.setName(newUsername);
+            userRepository.save(user);
+            return user;
+        }
+        return null;
     }
 }
